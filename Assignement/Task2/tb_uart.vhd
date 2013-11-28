e38152a0757d23a4bcd8152f72425ea2cac0178c
Gernot Vormayr <gvormayr@gmail.com>
2013-11-28 17:14:16 +0100

    added new test value

--- Assignement/Task2/tb_uart.vhd
+++ Assignement/Task2/tb_uart.vhd
@@ -33,13 +33,14 @@ begin
         rst_i <= '0';
         wait for 1*333 ns;
         Din_i <= X"55";
-        ld_i <= '1';
-        wait for 1*333 ns;
-        ld_i <= '0';
-        wait for 100000*333 ns;
+        wait for 4 ms;
+        Din_i <= X"CD";
+        wait for 4 ms;
         assert false report "done" severity failure;
         wait;
     end process;
+
+    rx_i <= tx_o; --loop back
 
     uart_i: entity work.uart_top_shell
     port map(
