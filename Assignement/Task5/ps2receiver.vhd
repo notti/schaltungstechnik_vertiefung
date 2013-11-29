a73a7e3a867ec7e2f6577df685cac120642047e9
Gernot Vormayr <gvormayr@gmail.com>
2013-11-29 19:47:47 +0100

    initial ps2receiver

--- Assignement/Task5/ps2receiver.vhd
+++ Assignement/Task5/ps2receiver.vhd
@@ -3,6 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ps2Receiver is
+    generic(
+        TIMEOUT  = 5000 -- = 100us at 50Mhz
+    );
 	port(
 		clk         : in  std_logic;
 		rst         : in  std_logic;
@@ -21,15 +24,87 @@ entity ps2Receiver is
 end entity ps2Receiver;
 
 architecture RTL of ps2Receiver is
-	
+    signal ps2Clk_sync : std_logic_vector(1 downto 0);
+    signal ps2Clk_dly  : std_logic;
+    signal ps2Clk_fall : std_logic;
+
+    type state_type is (idle, wait_start, shift, check); 
+    signal fsm : state_type;
+
+    signal data : std_logic_vector(10 downto 0);
+    signal bit_cnt : integer;
+
+
+    signal timeout_cnt : integer;
 
 begin
+    ps2Clk_synchronizer: process(clk)
+    begin
+        if rising_edge(clk) then
+            ps2Clk_sync <= ps2Clk & ps2Clk_sync(1);
+        end if;
+    end process ps2Clk_synchronizer;
 
--- Implement the FSM of the receiver here !
--- Asyncronous reset
--- Capture falling edges of th clock signal
--- Implement an accurate receive timeout
--- Set syncronization signals for the communication with other modules
+    ps2Clk_delay: process(clk)
+    begin
+        if rising_edge(clk) then
+            ps2Clk_dly <= ps2Clk_sync(0);
+        end if;
+    end process ps2Clk_delay;
 
+    ps2Clk_fall <= (not ps2Clk_sync(0)) and ps2Clk_dly;
+
+    timeout: process(clk, rst)
+    begin
+        if rst = '1' then
+            timeout_cnt <= 0;
+        elsif rising_edge(clk) then
+            if ps2Clk_fall = '1' or timeout_cnt = TIMEOUT then
+                timeout_cnt <= 0;
+            else
+                timeout_cnt <= timeout_cnt + 1;
+            end if;
+        end if;
+    end process;
+
+    fsm_p: process(clk, rst)
+    begin
+        if rst = '1' then
+            fsm <= idle;
+            bit_cnt <= 0;
+            data <= (others => '1');
+            rxData <= (others => '0');
+        elsif rising_edge(clk) then
+            case fsm is
+                when idle =>
+                    if dataFetched = '1' then
+                        fsm <= wait_start;
+                    end if;
+                when wait_start =>
+                    if ps2Clk_fall = '1' and ps2Data = '0' then
+                        fsm <= shift;
+                        bit_cnt <= 0;
+                        data <= (10 => '0', others => '1')
+                    end if;
+                when shift => 
+                    if timeout_cnt = TIMEOUT then
+                        fsm <= idle;
+                    end if;
+                    if ps2Clk_fall = '1' then
+                        bit_cnt <= bit_cnt + 1;
+                        data <= ps2Data & data(10 downto 1);
+                        if bit_cnt = 11 then
+                            fsm <= check;
+                        end if;
+                    end if;
+                when check =>
+                    fsm <= idle;
+                    if (xor data(9 downto 1)) = '1' and data(0) = '0' and data(10) = '1'  then
+                        rxData <= data(9 downto 2);
+                        dataReady <= '1';
+                    end if;
+            end case;
+        end if;
+    end process fsm_p;
 
 end architecture RTL;
