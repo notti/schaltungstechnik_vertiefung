2a8f9bd9ba6c4069e46ca5b95d3e187aade3d2e7
Gernot Vormayr <gvormayr@gmail.com>
2013-11-29 18:44:13 +0100

    initial pwm implementation

--- Assignement/Task4/pwm.vhd
+++ Assignement/Task4/pwm.vhd
@@ -1,9 +1,11 @@
 library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-use ieee.std_logic_1164.all;
-
 entity pwmUnit is
+    generic(
+       div : natural := 7; 
+    )
 	port(
 		clk_10 : in  std_logic;
 		rst    : in  std_logic;
@@ -12,15 +14,37 @@ entity pwmUnit is
 	);
 end entity pwmUnit;
 
+-- Led frequency 1kHz 256 Steps
+-- 50 Mhz / 128 = 390625 Hz / 256 Steps = 1526 Hz
+
 architecture RTL of pwmUnit is
+    signal freq_cnt : unsigned(div-1 downto 0);
+    signal pwm_cnt : unsigned(7 downto 0);
+    signal duty_reg : std_logic_vector(7 downto 0);
 begin
-	
--- Implement the PWM process here !
 
--- cover the reset signal asycronously
--- Pay attention on covering all possible input values correctly 
--- Use a counter register and compare the counter to your duty input vector
--- New duty cycle values are cathed after a counter overflow
+    clk_div: process(clk, rst)
+    begin
+        if rst = '1' then
+            freq_cnt <= (others => '0');
+        elsif rising_edge(clk) then
+            freq_cnt <= freq_cnt + 1;
+        end if;
+    end process;
 
+    pwm: process(clk, rst)
+    begin
+        if rst = '1' then
+            pwm_cnt <= (others => '0');
+            duty_reg <= (others => '0');
+        elsif rising_edge(clk) and freq_cnt = 0 then
+            pwm_cnt <= pwm_cnt + 1;
+            duty_reg <= duty;
+        end if;
+    end process;
+
+    outSig <= '0' when duty_reg = 0 else
+              '1' when pwm_cnt <= duty_reg else
+              '0';
 
 end architecture RTL;
