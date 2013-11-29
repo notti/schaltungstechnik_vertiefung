f2c5a22ba0fe38a0f7bf7f0459f73d4aac2f1879
Gernot Vormayr <gvormayr@gmail.com>
2013-11-29 20:15:42 +0100

    bug fixes found during simulation

--- Assignement/Task5/ps2receiver.vhd
+++ Assignement/Task5/ps2receiver.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity ps2Receiver is
     generic(
-        TIMEOUT  = 5000 -- = 100us at 50Mhz
+        TIMEOUT  :integer := 5000 -- = 100us at 50Mhz
     );
 	port(
 		clk         : in  std_logic;
@@ -36,6 +36,17 @@ architecture RTL of ps2Receiver is
 
 
     signal timeout_cnt : integer;
+    function xor_many(input : std_logic_vector) return std_logic is
+             variable result : std_logic;
+             variable i : integer;
+    begin
+            result := '0';
+            for i in input'low to input'high
+            loop
+                    result := result xor input(i);
+            end loop;
+            return result;
+    end;
 
 begin
     ps2Clk_synchronizer: process(clk)
@@ -54,7 +65,7 @@ begin
 
     ps2Clk_fall <= (not ps2Clk_sync(0)) and ps2Clk_dly;
 
-    timeout: process(clk, rst)
+    timeout_process: process(clk, rst)
     begin
         if rst = '1' then
             timeout_cnt <= 0;
@@ -74,17 +85,19 @@ begin
             bit_cnt <= 0;
             data <= (others => '1');
             rxData <= (others => '0');
+            dataReady <= '0';
         elsif rising_edge(clk) then
             case fsm is
                 when idle =>
                     if dataFetched = '1' then
                         fsm <= wait_start;
+                        dataReady <= '0';
                     end if;
                 when wait_start =>
                     if ps2Clk_fall = '1' and ps2Data = '0' then
                         fsm <= shift;
                         bit_cnt <= 0;
-                        data <= (10 => '0', others => '1')
+                        data <= (10 => '0', others => '1');
                     end if;
                 when shift => 
                     if timeout_cnt = TIMEOUT then
@@ -93,14 +106,14 @@ begin
                     if ps2Clk_fall = '1' then
                         bit_cnt <= bit_cnt + 1;
                         data <= ps2Data & data(10 downto 1);
-                        if bit_cnt = 11 then
-                            fsm <= check;
-                        end if;
+                    end if;
+                    if bit_cnt = 10 then
+                        fsm <= check;
                     end if;
                 when check =>
                     fsm <= idle;
-                    if (xor data(9 downto 1)) = '1' and data(0) = '0' and data(10) = '1'  then
-                        rxData <= data(9 downto 2);
+                    if xor_many(data(9 downto 1)) = '1' and data(0) = '0' and data(10) = '1'  then
+                        rxData <= data(8 downto 1);
                         dataReady <= '1';
                     end if;
             end case;
