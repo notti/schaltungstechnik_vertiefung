98a7f0c68fdb02a4f657c151ec54bdc5f0a2ff04
Gernot Vormayr <gvormayr@gmail.com>
2013-11-29 19:01:33 +0100

    pwm fixes

--- Assignement/Task4/pwm.vhd
+++ Assignement/Task4/pwm.vhd
@@ -2,10 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library work;
+use work.all;
+
 entity pwmUnit is
     generic(
-       div : natural := 7; 
-    )
+       div : natural := 7
+    );
 	port(
 		clk_10 : in  std_logic;
 		rst    : in  std_logic;
@@ -20,26 +23,33 @@ end entity pwmUnit;
 architecture RTL of pwmUnit is
     signal freq_cnt : unsigned(div-1 downto 0);
     signal pwm_cnt : unsigned(7 downto 0);
-    signal duty_reg : std_logic_vector(7 downto 0);
+    signal duty_reg : unsigned(7 downto 0);
 begin
 
-    clk_div: process(clk, rst)
+    clk_div: process(clk_10, rst)
     begin
         if rst = '1' then
             freq_cnt <= (others => '0');
-        elsif rising_edge(clk) then
+        elsif rising_edge(clk_10) then
             freq_cnt <= freq_cnt + 1;
         end if;
     end process;
 
-    pwm: process(clk, rst)
+    pwm: process(clk_10, rst)
     begin
         if rst = '1' then
             pwm_cnt <= (others => '0');
-            duty_reg <= (others => '0');
-        elsif rising_edge(clk) and freq_cnt = 0 then
+        elsif rising_edge(clk_10) and freq_cnt = 0 then
             pwm_cnt <= pwm_cnt + 1;
-            duty_reg <= duty;
+        end if;
+    end process;
+
+    duty_register: process(clk_10, rst)
+    begin
+        if rst = '1' then
+            duty_reg <= (others => '0');
+        elsif rising_edge(clk_10) and pwm_cnt = 0 then
+            duty_reg <= unsigned(duty);
         end if;
     end process;
 
